ff837f373b3fa74877c02d9f0b2cd58d4d83848a
BAYYA SUBRAHMANYA NARAYANA SWAROOP <swaroopbayya@gmail.com>
2019-06-10 15:22:56 +0530

    removed blank lines

--- src/resume.py
+++ src/resume.py
@@ -13,15 +13,10 @@ class JobDescription:
     def __init__(self, path):
 
         self.path = path
-
         self.summary = None
-
         self.experience = None
-
         self.education_details = None
-
         self.corpus = list()
-
         self.parse()
 
     def parse(self):
@@ -34,11 +29,8 @@ class JobDescription:
             text = file.read()
 
         self.summary = TextPreprocessor().text_cleaning(text, 'Summary(.*)')
-
         self.experience = TextPreprocessor().text_cleaning(text, 'Experience(.*?)Education')
-
         self.education_details = TextPreprocessor().text_cleaning(text, 'Education(.*)')
-
         self.corpus.append(self.summary)
 
 
@@ -47,17 +39,11 @@ class Resume:
     def __init__(self, path):
 
         self.path = path
-
         self.summary = None
-
         self.corpus = list()
-
         self.education_details = None
-
         self.experience = None
-
         self.value = None
-
         self.parse()
 
     def compare_with(self, obj):
@@ -68,7 +54,6 @@ class Resume:
         """
 
         self.corpus.append(' '.join(obj.corpus))
-
         self.value = self.score(self.corpus, obj)
 
         return self.value
@@ -78,7 +63,6 @@ class Resume:
         appends id to the score of every individual's resume and returns a list of id and score.
         :return:
         """
-
         path = str(self.path).replace('.pdf', '')
 
         return [path, self.value]
@@ -91,7 +75,6 @@ class Resume:
         """
 
         text = ' '.join(text)
-
         text = '\n'.join([line for line in text.splitlines() if line])
 
         return text
@@ -103,20 +86,15 @@ class Resume:
         :return:
         """
         logging.propagate = False
-
         logging.getLogger().setLevel(logging.ERROR)
 
         with open(self.path, 'rb') as file:
             text = slate.PDF(file)
 
         text = self.modify(text)
-
         self.summary = TextPreprocessor().text_cleaning(text, 'Summary(.*)')
-
         self.corpus.append(self.summary)
-
         self.experience = TextPreprocessor().text_cleaning(text, 'Experience(.*?)Education')
-
         self.education_details = TextPreprocessor().text_cleaning(text, 'Education(.*?)gmail')
 
     def score(self, corpus, obj):
@@ -129,11 +107,8 @@ class Resume:
         """
 
         cv = CountVectorizer(max_features=None)
-
         bag_of_words = cv.fit_transform(corpus).toarray()
-
         value = cosine_similarity(bag_of_words)
-
         value[0][1] = value[0][1] + self.experience / (obj.experience * 10)
 
         return value[0][1]
@@ -152,10 +127,10 @@ class SortId:
         length_list = len(id_list)
 
         if rank != 0:
-
             if rank > 0:
 
                 return [id_list[l] for l in range(0, rank)]
+
             else:
 
                 rank = length_list - abs(rank)
@@ -183,27 +158,18 @@ class TextPreprocessor:
         if regex == 'Experience(.*?)Education':
 
             raw_text = re.sub('[^a-zA-Z0-9]', ' ', raw_text)
-
             raw_text = re.findall(regex, raw_text)
-
             raw_text = ' '.join(raw_text)
-
             years = re.findall('([0-9]{1,2}).? years?', raw_text)
-
             months = re.findall('([0-9]{1,2}) months?', raw_text)
 
             return sum(list(map(int, years))) + (sum(list(map(int, months))) / 12)
 
         raw_text = re.sub('[^a-zA-Z]', " ", raw_text).split()
-
         raw_text = ' '.join([word for word in raw_text if word not in set(stopwords.words('english'))])
-
         special_words = ['machine', 'learning', 'artificial', 'intelligence', 'deep', 'learning']
-
         abbrev = {'ml': 'machinelearning', 'ai': 'artificialintelligence', 'dl': 'deeplearning'}
-
         summary = re.findall(regex, raw_text)
-
         len_of_text = len(summary)
 
         for i in range(len_of_text):
@@ -226,9 +192,7 @@ class TextPreprocessor:
 if __name__ == '__main__':
 
     jobDescription = JobDescription('/Users/swaroop/Desktop/swaroop/jds/jobDescription1.txt')
-
     pathlist = Path('/Users/swaroop/Desktop/swaroop/resumes').glob('*.pdf')
-
     id_list = list()
 
     for file in pathlist:
@@ -237,5 +201,4 @@ if __name__ == '__main__':
         id_list.append(resume.id())
 
     sort_id = SortId()
-
     print(sort_id.find(sort_id.sort_scores(id_list)))
